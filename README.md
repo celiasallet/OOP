4ef37b325ed6c425b7279dc603aaa1ae3e2bf69a
celiasallet <celiasallet@yahoo.fr>
2024-09-05 11:58:27 +0200

    usecase3 js

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 <img src="note2.heic" alt="Alt text" title="links" width="600px">
 
-**USE2**
+**USE1 BIS**
 
 <img src="note3.jpeg" alt="Alt text" title="links" width="600px">
+
+**USE2**
+
+<img src="note4.jpeg" alt="Alt text" title="links" width="600px">
---
+++ uses 3/use3.js
@@ -0,0 +1,32 @@
+class itemBasket {
+    constructor(item, qty, price, tax) {
+        this.item = item;              
+        this.qty = qty;       
+        this.price = price; 
+        this.tax = tax;     
+    }
+
+    soldes = 0.5;  
+
+    percentOff() {
+        return this.price * (1 - this.soldes);
+    }
+
+    totalBasket() {
+        return this.qty * this.percentOff();
+    } 
+
+    taxCost() {
+        return this.totalBasket() * this.tax;
+    }
+}
+
+const bananas = new itemBasket("Bananas", 6, 1.0, 0.06);
+const apples = new itemBasket("Apples", 3, 1.5, 0.06);
+const wine = new itemBasket("Wine", 2, 10.0, 0.21);
+
+const totalCost = bananas.totalBasket() + apples.totalBasket() + wine.totalBasket();
+const totalTax = bananas.taxCost() + apples.taxCost() + wine.taxCost();
+
+console.log('Total cost: ' + totalCost.toFixed(2) + '€');
+console.log('Total tax: ' + totalTax.toFixed(2) + '€');
